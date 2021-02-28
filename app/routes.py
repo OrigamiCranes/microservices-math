7218f722d2bd695d538e416408244824a3738cc3
OrigamiCranes <coding@jackmckeon.co.uk>
2021-02-28 16:47:36 +0000

    Completion of MVP

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from pandas.io.json import json_normalize
+
+from app import app
+from flask import render_template, request
+import pandas, json
+
+
+@app.route('/math/average', methods=['GET'])
+def average():
+    data_block = request.get_json()
+
+    data_frame = pandas.DataFrame.from_dict(data_block, orient='columns')
+
+    data_frame['MA-5'] = data_frame.rolling(window=5)['Close'].mean()
+
+    math_block = data_frame['MA-5'].to_json(orient='columns')
+    math_block = json.loads(math_block)
+    math_block = json.dumps({'MA-5': math_block})
+
+    with open('data.json', 'w') as f:
+        json.dump(math_block, f)
+    return math_block
+
+
+
+
